80a302f0f54d0f6aa7535a6ce36b1bc12e407877
NatanOliveiraslv <natanoliveiraslv14@gmail.com>
2025-06-24 21:25:40 -0300

    add component collaborator

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,17 @@
+import './Collaborator.css';
+
+const Collaborator = () =>{
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/NatanOliveiraslv.png' alt='Natan Oliveira da Silva'/>
+            </div>
+            <div className='rodape'>
+                <h4>Natan Oliveira</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Collaborator;
